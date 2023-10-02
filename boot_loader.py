fb50eb85c172ae263cc9fbfc90b4c017b20244f4
LZong <lzong.tw@gmail.com>
2023-10-03 03:12:26 +0800

    .

--- boot_loader.py
+++ boot_loader.py
@@ -1,3 +1,8 @@
 import subprocess
+import win32gui
 
+hwnd = win32gui.GetForegroundWindow()  # Get handle to the current window
+
+# Set AppUserModelID for the window
+win32gui.SetProp(hwnd, "AppUserModelID", "Confession.Bootloader")
 subprocess.run(['python', 'main.py'], check=True)

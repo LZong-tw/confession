e0c221c61c850556ef88199ca49fad45d92bf6ef
LZong <lzong.tw@gmail.com>
2023-10-09 22:47:23 +0800

    .

--- confession/arduino_service.py
+++ confession/arduino_service.py
@@ -17,10 +17,10 @@ def find_arduino_port():
 
 def arduino_service(door_queue_for_screen, door_queue_for_audio, welcome_queue, block_queue, port="COM3"):
     ser = False
-    port = find_arduino_port()
     # Define the serial port and baud
     if ser:
         try:
+            port = find_arduino_port()
             ser = serial.Serial(port, 9600)
         except Exception as e:
             print("Arduino not connected")
